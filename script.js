6c63056f3e24dc92466142fa1126c47d029da9ed
lyskz <lyski.zach@icloud.com>
2023-03-07 22:49:22 -0800

    finish touches and made forecast display upon click and use data from fetch

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ var temp = document.getElementById("temp");
 var wind = document.getElementById("wind");
 var humidity = document.getElementById("humidity");
 var listBtn = "";
+//const myData = JSON.parse(localStorage.getItem("savedData"));
+var savedData = "";
+
 
 $(".search-btn").on("click", getWeatherData);
 
@@ -48,14 +51,12 @@ function getWeatherData() {
             var newtoday = today.replace("GMT-0800 (Pacific Standard Time)", "");
             // appends this button into html element
             var button = document.createElement("button");
-            var buttontxt = document.createTextNode(data.name);
-            //var className = data.name;
-            button.appendChild(buttontxt);
-            button.classList.add("list-btn", data.name);
+            button.textContent = data.name;
+            button.classList.add("list-btn");
             cityList.append(button);
-            console.log(buttontxt.data);
+            // for the listed items
             listBtn = $(".list-btn");
-            listBtn.on("click", displayData);
+            listBtn.on("click", savedWeather);
             
             // sets the large display of current date and local time
             currentCity.textContent = data.name + " - " + newtoday;
@@ -63,9 +64,7 @@ function getWeatherData() {
             wind.textContent = "Wind: " + data.wind.speed + " MPH";
             humidity.textContent = "Humidity: " + data.main.humidity + "%";
             //saves latitiude and longitude of city name
-            localStorage.setItem(data.name + "lat", lat);
-            localStorage.setItem(data.name + "lon", lon);
-            
+            //localStorage.setItem("savedData", JSON.stringify(savedData));
         })
         // this sets 5 day forecast
         fetch(getFWetURL)
@@ -74,21 +73,56 @@ function getWeatherData() {
         })
         .then(function (data) {
             console.log(data);
-        })
-        // display data upon click event for saved locations
-        function displayData() {
-            if(this.target === data.name) {
-                console.log("success");
-                
+            for(i=0; i<5; i++) {
+                var forecastDay = document.createElement("div");
+                forecastDay.classList.add("day");
+                var listed = document.createElement("ul");
+                forecastDay.append(listed);
+                var date = document.createElement("li");
+                date.textContent = dayjs().add(i, "day").format("MMM D, YYYY");
+                listed.append(date);
+                var temp = document.createElement("li");
+                temp.textContent = "Temp: " + data.list[8*i].main.temp + "'F";
+                listed.append(temp);
+                var wind = document.createElement("li");
+                wind.textContent = "Wind: " + data.list[8*i].wind.speed + "mph";
+                listed.append(wind);
+                var humidity = document.createElement("li");
+                humidity.textContent = "Humidity: " + data.list[8*i].main.humidity + "%";
+                listed.append(humidity);
+                $(".five-day-forecast").append(forecastDay);
             }
-        }
-
+        })
+        
     
     })
 
 }
+//const items = { ...localStorage };
+//console.log(items);
 
-
-
-
+// display data upon click event for saved location
+// need to connect lat and lon to the city name and buttons
+function savedWeather() {
+    //console.log(JSON.parse(localStorage.getItem("savedData")));
+    // splice the lat and lon to get each value
+    var currentWetURL = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + APIKey + "&units=imperial";
+    fetch(currentWetURL)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function update (data) {
+            d = new Date();
+            localTime = d.getTime();
+            localOffset = d.getTimezoneOffset() * 60000;
+            utc = localTime + localOffset;
+            var input = utc + (1000 * data.timezone);
+            var today = new Date(input).toString();
+            var newtoday = today.replace("GMT-0800 (Pacific Standard Time)", "");
+            currentCity.textContent = data.name + " - " + newtoday;
+            temp.textContent = "Temperature: " + data.main.feels_like + " Degree";
+            wind.textContent = "Wind: " + data.wind.speed + " MPH";
+            humidity.textContent = "Humidity: " + data.main.humidity + "%";
+})
+}
 
